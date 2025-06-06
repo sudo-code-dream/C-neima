d3d5e78dc43ed7a732e40ed3d3d20d6f8c69dc88
kennethdean <kennethjrdean99@gmail.com>
2025-06-06 18:53:56 +0800

    fix: add missing description and id fields in manifest.json

--- app/head.tsx
+++ app/head.tsx
@@ -18,30 +18,30 @@ export default function Head() {
       <link
         rel='apple-touch-icon'
         sizes='152x152'
-        href='/icons/touch-icon-ipad.png'
+        href='/icon512_maskable.png'
       />
       <link
         rel='apple-touch-icon'
         sizes='180x180'
-        href='/icons/touch-icon-iphone-retina.png'
+        href='/icon512_maskable.png'
       />
       <link
         rel='apple-touch-icon'
         sizes='167x167'
-        href='/icons/touch-icon-ipad-retina.png'
+        href='/icon512_maskable.png'
       />
 
       <link
         rel='icon'
         type='image/png'
         sizes='32x32'
-        href='/icons/favicon-32x32.png'
+        href='/icon512_maskable.png'
       />
       <link
         rel='icon'
         type='image/png'
         sizes='16x16'
-        href='/icons/favicon-16x16.png'
+        href='/icon512_maskable.png'
       />
       <link rel='manifest' href='/manifest.json' />
       <link
